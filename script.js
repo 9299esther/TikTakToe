ceb9b3b0a6c7ee49e026a04acb35f9a2080e34e5
9299esther <36642778+9299esther@users.noreply.github.com>
2019-12-18 16:46:07 +0200

    18/12/19

--- script.js
+++ script.js
@@ -9,8 +9,7 @@ let input2 = document.getElementById("input2")
 let name1 = 'player1'
 let name2 = 'player2'
 
-
-if (!name1 || !name2) {
+if (!name1 || !name2) {//שמות שחקנים
     b1.onclick = function () {
         name1 = input1.value
         player1.innerHTML = `<div>${name1}</br> <b>O</b></div>`
@@ -24,22 +23,59 @@ else {
     player1.innerHTML = `<div>${name1}</br> O</div>`
     player2.innerHTML = `<div>${name2}</br> X</div>`
 }
+
 let turn = 1
 
 /* let column = document.getElementsByClassName('column')
  */
-var players = document.getElementById('board')
-players.addEventListener('click', function (ev) {
-    debugger
-    ev.target.classList.add(`${turn % 2 ? "ex" : "Circle"}`)
+let players = document.getElementById('board')
+if(turn%2){//תור מי
 
-if(turn==1)
-    turn=2
-else
-    turn=1
+}
+
+let timer = document.getElementById('timer')
+let Seconds = 0
+let Minutes = 0
+
+/* setInterval(function () {//טימר
+    if (Seconds == 59) {
+        Seconds = 0
+        if (Minutes == 1) {        
+                alert('Time up')
+        }
+        else
+            Minutes++
+    }
+    else
+        Seconds++
+     if (Seconds < 10 && Minutes < 10)
+        timer.innerText = '0' + Minutes + ':' + '0' + Seconds
+    else if (Minutes < 10 )
+        timer.innerText =  '0' + Minutes + ':' + Seconds
+   else
+        timer.innerText =  Minutes + ':' + Seconds
+ }, 1000); */
+
+
+players.addEventListener('click', function (ev) {
+    
+    ev.target.classList.add(`${turn % 2 ? "ex" : "Circle"}`)
+    turn++
+    Seconds = 0
+    Minutes = 0
 })
-//חדש
-let newb = document.querySelector('New')
+// משחק חדש
+ /* 
+let newGame = document.getElementById('New')
+
+newGame.onclick = function(){
+    alert(newGame)
+    newGame.classList.remove("ex")
+    alert(222)
+} */
+
+
+
 // ניקוי לוח
 
 // איפוס טיימר
